477a8817e05ee2159c948ae106b6bcd024081a02
Daniel De Michele <carpdemichele@gmail.com>
2020-11-04 22:39:46 -0300

    Change message from varchar to text

--- keepy.sql
+++ keepy.sql
@@ -4,7 +4,7 @@ USE keepy;
 
 CREATE TABLE `messages` (
   `id` int(11) AUTO_INCREMENT NOT NULL,    
-  `message` varchar(500) NOT NULL,
+  `message` text() NOT NULL,
   `channelId` varchar(300) NOT NULL,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8mb4;
